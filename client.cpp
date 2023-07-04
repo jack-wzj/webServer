ebda7ff94e724214d208e711bd2bc3edab1fa313
zjwei <919576866@qq.com>
2023-07-04 23:11:41 +0800

    fix: 改写客户端和服务器代码，实现单连接的echo服务器

--- client.cpp
+++ client.cpp
@@ -2,12 +2,17 @@
 #include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
+#include <unistd.h>
+#include "util.h"
 
 using namespace std;
+
+void errif(bool, const char *);
 
 int main() {
     // 建立一个socket套接字，对外提供一个网络通信接口
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(sockfd == -1, "socket create error");
     // 服务器地址
     struct sockaddr_in serv_addr;
     // 初始化
@@ -16,8 +21,40 @@ int main() {
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8099);
+
     // 连接服务器
-    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "connect error!" );
     cout << "connect to server" << endl;
+
+    // 向服务器发送数据
+    while (true) {
+        // 缓冲区初始化
+        char buffer[1024];
+        bzero(&buffer, sizeof(buffer));
+        // 读取用户输入
+        scanf("%s", buffer);
+        // 向服务器发送数据
+        ssize_t write_bytes = write(sockfd, buffer, sizeof(buffer));
+        if (write_bytes == -1) {
+            cout << "socket disconnected, cant write" << endl;
+            break;
+        }
+        // 读取服务器返回的数据
+        bzero(&buffer, sizeof(buffer));
+        ssize_t read_bytes = read(sockfd, buffer, sizeof(buffer));
+        if (read_bytes == -1) {
+            close(sockfd);
+            errif(true, "read error!");
+        }
+        else if (read_bytes == 0) {
+            close(sockfd);
+            cout << "server closed" << endl;
+            break;
+        }
+        else if (read_bytes > 0) {
+            cout << "recv: " << buffer << endl;
+        }
+    }
+    close(sockfd);
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -2,29 +2,57 @@
 #include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
+#include <unistd.h>
+#include "util.h"
 
 using namespace std;
 
+void errif(bool, const char *);
+
 int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    errif(sockfd == -1, "socket create error");
     // 服务器地址
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8099);
+
     // 将socket地址与文件描述符绑定
-    bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    errif(bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "bind error!");
     // 监听端口
-    cout << "listen on port 8099: " << SOMAXCONN << endl; // 4096
-    listen(sockfd, SOMAXCONN);
+    // cout << "listen on port 8099: " << SOMAXCONN << endl; // 4096
+    errif(listen(sockfd, SOMAXCONN) == -1, "listen error!");
     // 接受客户端连接
     struct sockaddr_in client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
     bzero(&client_addr, sizeof(client_addr));
     // accept()函数会阻塞，直到有客户端连接
     int client_sockfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len);
+    errif(client_sockfd == -1, "accept error!");
     printf("new client fd %d IP: %s Port: %d\n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+    
+    // 读取客户端发送的数据
+    while (true) {
+        char buffer[1024];
+        bzero(&buffer, sizeof(buffer));
+        ssize_t read_bytes = read(client_sockfd, buffer, sizeof(buffer));
+        if (read_bytes == 0) {
+            printf("client %d closed\n", client_sockfd);
+            close(client_sockfd);
+            break;
+        }
+        else if (read_bytes == -1) {
+            close(client_sockfd);
+            errif(true, "read error!");
+        }
+        else if (read_bytes > 0) {
+            printf("recv: %s\n", buffer);
+            write(client_sockfd, buffer, sizeof(buffer));
+        }
+    }
+    close(sockfd);
     
     return 0;
 }
